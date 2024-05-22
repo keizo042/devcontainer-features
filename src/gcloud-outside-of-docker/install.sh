9d7a871c04bc2b598e3a070cd1c5d9ad10a2e240
keizo042 <keizo042dev@gmail.com>
2024-05-22 09:45:31 +0900

    fix

--- src/gcloud-outside-of-docker/install.sh
+++ src/gcloud-outside-of-docker/install.sh
@@ -3,3 +3,5 @@ set -e
 
 echo "Activating feature 'kubeconfig-outside-of-docker'"
 
+apt update -y 
+apt install -y fswatch rsync
--- src/kubeconfig-outside-of-docker/install.sh
+++ src/kubeconfig-outside-of-docker/install.sh
@@ -3,3 +3,6 @@ set -e
 
 echo "Activating feature 'kubeconfig-outside-of-docker'"
 
+
+apt update -y
+apt install -y fswatch rsync
