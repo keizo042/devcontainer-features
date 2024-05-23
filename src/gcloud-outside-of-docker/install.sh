6f587fc71d7296caa6724f56cfaf222e22d91112
Koichi Nakanishi <nakanishi.koichi@enechain.co.jp>
2024-05-23 11:27:37 +0900

    use /media/home

--- src/gcloud-outside-of-docker/install.sh
+++ src/gcloud-outside-of-docker/install.sh
@@ -4,4 +4,4 @@ set -e
 echo "Activating feature 'kubeconfig-outside-of-docker'"
 
 mkdir -p /home/$_REMOTE_USER/.config
-ln -s /var/tmp/home/.config/gcloud /home/$_REMOTE_USER/.config/
+ln -s /media/home/.config/gcloud /home/$_REMOTE_USER/.config/
